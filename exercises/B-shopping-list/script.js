3db131a341c27948dfa2174a4e7c50a153ec245c
Texx45 <tcalderbank@hotmail.com>
2021-07-26 18:32:38 +0100

    completed B-shoppingList

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,7 +1,18 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+function shoppingList(shoppingList) {
+  let content = document.querySelector("#content"); // grab element
+  let createParagraph = document.createElement("p"); // create p tag
+
+  shoppingList.forEach((item) => {
+    // iterate through array
+    createParagraph.innerText += `\n${item}`; // create list of item for each iteration
+    content.appendChild(createParagraph); // render to screen
+  });
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
 
 shoppingList(shopping);
+
+// * You should use an unordered list: `<ul>`.
+// * Each item from the array should be in a `<li>` tag.
+// * All of your HTML should go inside the `<div>` with the id **"content"**.
