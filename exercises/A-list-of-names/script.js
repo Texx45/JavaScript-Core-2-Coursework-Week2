9b3386e6e2f3ee86e3c167072ca81587ab3ab65b
Texx45 <tcalderbank@hotmail.com>
2021-07-26 15:37:26 +0100

    completed A-listOfNames

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,23 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+  let renderDiv = document.createElement("div");
+  for (const person in people) {
+    peoplesNames = people[person].name;
+    peoplesJobs = people[person].job;
+
+    const markup = `
+    
+ <div class="person-description">
+    <h1>
+        ${peoplesNames}
+    </h1>
+    <h2>
+        ${peoplesJobs}
+    </h2>
+ </div>
+    `;
+    document.body.innerHTML += markup;
+  }
 }
 
 let people = [
